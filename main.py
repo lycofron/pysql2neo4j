5e534a13e45bd674f1160c49e2674ca6749bfeb0
lycofron <lycofron@gmail.com>
2014-11-25 16:46:09 +0200

    Half-working
    Creates nodes, creates relationships but it times out after 75K rels
    created for unknown reasons.

--- main.py
+++ main.py
@@ -5,39 +5,91 @@ Created on 24 Apr 2013
 '''
 
 ##Yeah, this has nothing to do with the rest of the code, this is just a code testing playground.
-
+import sys
+import string
 from sqlalchemy import create_engine, MetaData, Table, select
 from sqlalchemy.engine import reflection
-from py2neo import node, rel
-from py2neo import neo4j
-from pysql2neo4j.pysql2neo4j import SrcDBProcessor
+from py2neo import Node, Relationship
+from py2neo import Graph
+from py2neo.cypher import cypher_escape
+from pysql2neo4j.pysql2neo4j import SqlDbProcessor
 
-sourcedb="mysql+mysqlconnector://worlduser:123456@127.0.0.1/world?charset=utf8"
-graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+#sourcedb="mysql+mysqlconnector://sakilauser:123456@127.0.0.1/sakila?charset=utf8"
+graph_db = Graph("http://localhost:7474/db/data/")
 
 if __name__ == '__main__':
-#    batch = neo4j.WriteBatch(graph_db)
     cmdCount=0
-    cmdCountCycle=1000
+    cmdCountCycle=150
     cmdCountTotal=0
+    nodeList=[]
     
-    #Phase 1: Create Nodes
-    for t in SrcDBProcessor().iterTables():
+    #Phase 1: Create Nodes with their primary keys
+    for t in SqlDbProcessor().iterTables:
         print t.tablename
+        tblLabel = t.tablename
+        pkLabel = t.tablename + "_PK"
         cmdCount=cmdCount+1
         for row in t.iterRows():
             mainNodeData={}
-            for c in t.esscols: mainNodeData[c]=row[c]
-#            n=batch.create(mainNodeData)
-#            batch.add_labels(n,t)
+            for c in t.allcols: mainNodeData[c]=row[c]
+            n=Node(tblLabel,**mainNodeData)
+            nodeList.append(n)
+               
             cmdCount=cmdCount+1
-            if cmdCount > cmdCountCycle:
-#                batch.submit()
+               
+            if cmdCount >= cmdCountCycle:
+                try:
+                    graph_db.create(*nodeList)
+                    nodeList=[]
+                except Exception as ex:
+                    print nodeList
+                    print ex.message
+                    sys.exit(-1)
                 cmdCountTotal = cmdCountTotal + cmdCount
                 cmdCount = 0
                 print "Sent %d commands." % cmdCountTotal
-        if cmdCount>0:
-#            batch.submit()
-            print "Sent %d leftover commands." % cmdCount
+    if cmdCount>0:
+        graph_db.create(*nodeList)
+        nodeList=[]
+        cmdCountTotal = cmdCountTotal + cmdCount
+    print "Total %d commands sent." % cmdCountTotal
+    
+    
+    #Phase 2: Relate nodes
+    relList = []
+    cmdCount=0
+    for t in SqlDbProcessor().iterTables:
+        if t.foreignKeys:
+            print "Processing %s." % t.tablename
+            for node in graph_db.find(label=t.tablename):
+                for fk in t.foreignKeys:
+                    conditions = string.join(["%s:%s" % (col, node[col]) for col in fk.cols if node[col]],",")
+                    if not conditions: continue
+                    cypher_query = "match (n:%s{%s}) return n;" % (fk.refTable.tablename,conditions)
+                    records = graph_db.cypher.execute(cypher_query)
+                    subgraph = records.to_subgraph()
+                    assert len(subgraph.nodes)==1
+                    targetNode = next(iter(subgraph.nodes))
+                    relList.append(Relationship(node,t.tablename + "_RL_" + fk.refTable.tablename,targetNode))
+                    cmdCount=cmdCount+1
+                      
+                    if cmdCount >= cmdCountCycle:
+                        try:
+                            graph_db.create(*relList)
+                            relList=[]
+                        except Exception as ex:
+                            print relList
+                            print ex.message
+                            sys.exit(-1)
+                        cmdCountTotal = cmdCountTotal + cmdCount
+                        cmdCount = 0
+                        print "Sent %d commands." % cmdCountTotal
+        else:
+            print "Skipping %s..." % t.tablename
+    if cmdCount>0:
+        graph_db.create(*nodeList)
+        nodeList=[]
+        cmdCountTotal = cmdCountTotal + cmdCount
+
     print "Total %d commands sent." % cmdCountTotal
     print "Terminated"
--- pysql2neo4j/configman.py
+++ pysql2neo4j/configman.py
@@ -11,8 +11,7 @@ from sqlalchemy.engine import reflection
 from sqlalchemy import Table, Column, Integer
 from customexceptions import *
 
-
-from py2neo import neo4j
+from py2neo import Graph, Node
 
 class SourceDb:
     @classmethod
@@ -84,50 +83,46 @@ def getTestedSQLDatabase(dburi,tryWrite=False):
             raise DBInsufficientPrivileges("Exception while testing trivial operations in DB %s." % dburi)
     return engine, conn, insp
 
-    def getTestedNeo4jDB(graphDBurl):
-        '''Gets a Neo4j url and returns a GraphDatabaseService to the database
-        after having performed a few tests (connection, creation of nodes, creation of batch)
-        '''
-        try:
-            graph_db = neo4j.GraphDatabaseService(self.graphDbConnectionString)
-        except Exception as ex:
-            raise DBUnreadableException(ex,"Could not connect to graph database.")
+def getTestedNeo4jDB(graphDBurl):
+    '''Gets a Neo4j url and returns a GraphDatabaseService to the database
+    after having performed a few tests (connection, creation of nodes, creation of batch)
+    '''
+    try:
+        graph_db = Graph(graphDBurl)
+    except Exception as ex:
+        raise DBUnreadableException(ex,"Could not connect to graph database.")
+    
+#     try:
+#         r = graph_db.cypher.execute("start n=node(*) return count(n) as nodecount;")
+#         if r.records[0].nodecount>0:
+#             raise WorkflowException(Exception(),"Graph Database is not empty.")
+#     except Exception as ex:
+#         raise DBInsufficientPrivileges(ex,"Could not execute query to graph database.")
+    
+    try:
+        test_node = Node("TEST", data="whatever")
+        graph_db.create(test_node)
+        graph_db.delete(test_node)
+    except Exception as ex:
+        raise DBInsufficientPrivileges(ex,"Could not execute simple operations to graph database.")
+    
+#     try:
+#         batch = neo4j.WriteBatch(graph_db)
+#     except Exception as ex:
+#         raise DBInsufficientPrivileges(ex,"Could not get WriteBatch from graph database.")
         
-        try:
-            q = neo4j.CypherQuery(graph_db,"start n=node(*) where ID(n)>0 return count(n);")
-            r,_ = q.execute()
-            if r[0][0]>0:
-                raise WorkflowException(Exception(),"Graph Database is not empty.")
-        except Exception as ex:
-            raise DBInsufficientPrivileges(ex,"Could not execute query to graph database.")
-        
-        try:
-            test_node = graph_db.create({"data":"whatever"})[0]
-            fetched = graph_db.node(test_node.id)
-            fetched.delete()
-        except Exception as ex:
-            raise DBInsufficientPrivileges(ex,"Could not execute simple operations to graph database.")
-        
-        try:
-            batch = neo4j.WriteBatch(graph_db)
-        except Exception as ex:
-            raise DBInsufficientPrivileges(ex,"Could not get WriteBatch from graph database.")
-            
 
-        return graph_db
+    return graph_db
 
 class DBConnManager(object):
     ## Yes, indeed, these vars right below have no place here. 
     ## They were put there just for testing purposes and are to be removed ASAP.
-    sourcedb="mysql+mysqlconnector://worlduser:123456@127.0.0.1/world?charset=utf8"
-    tempdb="mysql+mysqldb://worlduser:123456@127.0.0.1/mig?charset=utf8"
+    sourcedb="mysql+mysqlconnector://sakilauser:123456@127.0.0.1/sakila?charset=utf8"
     graphDbConnectionString = "http://localhost:7474/db/data/"
     
     def __init__(self):
         # Get source database and test it
         self.srcengine, self.srcconn, self.srcinsp = getTestedSQLDatabase(self.sourcedb)        
-        # Get a temp db to work with intermediate data
-        self.tmpengine, self.tmpconn, self.tmpinsp = getTestedSQLDatabase(self.tempdb)        
-        self.destGraphDb = getTestedNeo4jDB(graphDbConnectionString)
+        self.destGraphDb = getTestedNeo4jDB(self.graphDbConnectionString)
         
             
